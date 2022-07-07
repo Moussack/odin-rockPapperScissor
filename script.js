9216556b6761b0265a113ea0c3edcae7e80fd41d
Moussack <reywahyono@gmail.com>
2022-07-07 14:19:16 +0700

    completer playRound function

---
+++ script.js
@@ -0,0 +1,36 @@
+function computerPlay() {
+   const comSelection = Math.floor(Math.random() * 3);
+   if (comSelection === 0) {
+      return 'rock';
+   } else if (comSelection === 1) {
+      return 'papper';
+   } else {
+      return 'scissor';
+   }
+}
+
+const playerSelection = 'papper';
+const computerSelection = computerPlay();
+
+function playRound(p, c) {
+   if (p === 'rock' && c === 'papper') {
+      console.log(`Player choose:${p} And Computer choose:${c}`);
+      return 'You Lose, Papper beats Rock';
+   } else if (p === 'rock' && c === 'scissor') {
+      console.log(`Player choose:${p} And Computer choose:${c}`);
+      return 'You Win, Rock beats scissor';
+   } else if (p === 'rock' && c === 'rock') {
+      console.log(`Player choose:${p} And Computer choose:${c}`);
+      return 'Tie';
+   } else if (p === 'papper' && c === 'rock') {
+      console.log(`Player choose:${p} And Computer choose:${c}`);
+      return 'You Win, Papper beats Rock';
+   } else if (p === 'papper' && c === 'scissor') {
+      console.log(`Player choose:${p} And Computer choose:${c}`);
+      return 'You Lose, Scissor beats Papper';
+   } else if (p === 'papper' && c === 'papper') {
+      console.log(`Player choose:${p} And Computer choose:${c}`);
+      return 'Tie';
+   }
+}
+console.log(playRound(playerSelection, computerSelection));
