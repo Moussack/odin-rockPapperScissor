676112d479625747863f8cf9b8a9e29f59be948b
Moussack <reywahyono@gmail.com>
2022-07-08 15:42:31 +0700

    implement player score try 2

--- script.js
+++ script.js
@@ -22,69 +22,71 @@ function playerPlay() {
    }
 }
 
-const computerSelection = computerPlay();
-const playerSelection = playerPlay();
-console.log(playRound(playerSelection, computerSelection));
+// const computerSelection = computerPlay();
+// const playerSelection = playerPlay();
+
+// console.log(playRound(playerSelection, computerSelection));
 
 // this function controls all Output scenario
 function playRound(p, c) {
-   let playerScore = 0;
-   let computerScore = 0;
-
-   console.log(`Player Score: ${playerScore}`);
-   console.log(`Computer Score: ${computerScore}`);
-
    if (p === 'rock' && c === 'papper') {
       console.log(`Player choose:${p} And Computer choose:${c}`);
-      computerScore = computerScore + 1;
       return 'You Lose, Papper beats Rock';
    } else if (p === 'rock' && c === 'scissor') {
       console.log(`Player choose:${p} And Computer choose:${c}`);
-      playerScore = playerScore + 1;
       return 'You Win, Rock beats scissor';
    } else if (p === 'rock' && c === 'rock') {
       console.log(`Player choose:${p} And Computer choose:${c}`);
-      playerScore = playerScore + 0;
-      computerScore = computerScore + 0;
       return 'Tie';
    } else if (p === 'papper' && c === 'rock') {
       console.log(`Player choose:${p} And Computer choose:${c}`);
-      playerScore = playerScore + 1;
       return 'You Win, Papper beats Rock';
    } else if (p === 'papper' && c === 'scissor') {
       console.log(`Player choose:${p} And Computer choose:${c}`);
-      computerScore = computerScore + 1;
       return 'You Lose, Scissor beats Papper';
    } else if (p === 'papper' && c === 'papper') {
       console.log(`Player choose:${p} And Computer choose:${c}`);
-      playerScore = playerScore + 0;
-      computerScore = computerScore + 0;
       return 'Tie';
    } else if (p === 'scissor' && c === 'papper') {
       console.log(`Player choose:${p} And Computer choose:${c}`);
-      playerScore = playerScore + 1;
       return 'You Win, Scissor beats Papper';
    } else if (p === 'scissor' && c === 'rock') {
       console.log(`Player choose:${p} And Computer choose:${c}`);
-      computerScore = computerScore + 1;
       return 'You Lose, Rock beats Scissor';
    } else if (p === 'scissor' && c === 'scissor') {
       console.log(`Player choose:${p} And Computer choose:${c}`);
-      playerScore = playerScore + 0;
-      computerScore = computerScore + 0;
       return 'Tie';
    }
 }
 
 // This function is to start the game
-/* function game() {
+function game() {
+   let pScore = 0;
+   let cScore = 0;
+
    for (let i = 0; i < 5; i++) {
       const computerSelection = computerPlay();
       const playerSelection = playerPlay();
-      console.log(playRound(playerSelection, computerSelection));
+      // console.log(playRound(playerSelection, computerSelection));
+      const result = playRound(playerSelection, computerSelection);
+
+      if (result.includes('Win')) {
+         console.log('WON!!! player got SCORE + 1 ');
+         pScore = pScore + 1;
+      } else if (result.includes('Lose')) {
+         console.log('LOSE!!!computer got SCORE + 1');
+         cScore = cScore + 1;
+      } else {
+         console.log('TIE!!! you got Tie SCORE + 0');
+         pScore = pScore + 0;
+         cScore = cScore + 0;
+      }
    }
+
+   console.log(`player score final ${pScore}`);
+   console.log(`computer score final ${cScore}`);
 }
 
-game(); */
+game();
 
 // NEXT TO DO = calculate players and computer score if they won each round and use prompt method to gather user input
